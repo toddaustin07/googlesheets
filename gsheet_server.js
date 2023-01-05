74c689a9b290ed512a4488052fcf8e7ae2f3f75b
Todd Austin <toddaustin07@yahoo.com>
2023-01-04 20:06:20 -0600

    Add files via upload

---
+++ gsheet_server.js
@@ -0,0 +1,63 @@
+
+/**********************************************************************/
+//MODIFY THE TWO ITEMS BELOW WITH YOUR GOOGLE KEYFILE AND GOOGLE DOC ID
+/**********************************************************************/
+const KEYFILE = './focus-electron-373720-e17f880e535a.json'
+const GOOGLEDOCID = '10lPneJc-zcPLAQqLJVrDRmIoMb_FptkHOFc3Krhak_Q'
+/**********************************************************************/
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = module.exports = express();
+server.use(bodyParser.json());
+
+const OURPORT = 8089
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const doc = new GoogleSpreadsheet(GOOGLEDOCID);
+
+let sheet = {}
+
+
+server.post('/', async (req, res) => {
+  
+  console.log ('Post received: ', req.body)
+  
+  res.writeHead(200);
+  res.end();
+  
+  let dateTime = new Date();
+  let dateTimestr = String(dateTime).replace(/GMT.+/, '')
+  
+  await sheet.addRow({ Date: dateTimestr, Event: req.body.event});
+  
+});
+
+
+
+async function initauth() {
+
+  const creds = require(KEYFILE);
+
+  await doc.useServiceAccountAuth(creds);
+  
+  await doc.loadInfo();
+  sheet = doc.sheetsByIndex[0];
+}
+
+
+
+initauth()
+  .then(result => {
+    server.listen(OURPORT, console.log(`Listening on port ${OURPORT}`));
+  })
+  .catch(error => {
+    console.log ("Error:", error);
+  })
+
+
+
+
+
