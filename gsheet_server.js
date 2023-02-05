af03c4a71be6a728c8a9c25cc0a02564d792fde6
Todd Austin <toddaustin07@yahoo.com>
2023-02-05 17:32:56 -0600

    Add files via upload

--- gsheet_server.js
+++ gsheet_server.js
@@ -18,23 +18,32 @@ const OURPORT = 8089
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const doc = new GoogleSpreadsheet(GOOGLEDOCID);
 
-let sheet = {}
-
 
 server.post('/', async (req, res) => {
   
-  console.log ('Post received: ', req.body)
+  console.log (`POST received with body: ${JSON.stringify(req.body)}`);
   
   res.writeHead(200);
   res.end();
   
-  let dateTime = new Date();
-  let dateTimestr = String(dateTime).replace(/GMT.+/, '')
+  let sheet = {};
+  if (req.body.sheet) {
+    sheet = doc.sheetsByTitle[req.body.sheet];
+  } else
+    sheet = doc.sheetsByIndex[0];
   
-  await sheet.addRow({ Date: dateTimestr, Event: req.body.event});
+  
+  let dateTime = new Date();
+  let dateTimestr = String(dateTime).replace(/GMT.+/, '');
+  
+  try {
+    await sheet.addRow({ Date: dateTimestr, Event: req.body.event});
+    
+  } catch (err) {
+    console.log ('\tERROR: Unable to add row to sheet');
+  }
   
 });
-
 
 
 async function initauth() {
@@ -44,11 +53,11 @@ async function initauth() {
   await doc.useServiceAccountAuth(creds);
   
   await doc.loadInfo();
-  sheet = doc.sheetsByIndex[0];
+  
 }
 
 
-
+console.log('\nGooglesheet Server for SmartThings Events v1.0\n')
 initauth()
   .then(result => {
     server.listen(OURPORT, console.log(`Listening on port ${OURPORT}`));
@@ -56,8 +65,3 @@ initauth()
   .catch(error => {
     console.log ("Error:", error);
   })
-
-
-
-
-
